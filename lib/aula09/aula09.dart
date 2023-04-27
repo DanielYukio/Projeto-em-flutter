04593e6833f138797bfd752630996cf8267f26ee
Daniel Yukio <danielyukio004@gmail.com>
2023-04-27 19:35:41 -0300

    aula09

---
+++ lib/aula09/aula09.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Aula09 extends StatefulWidget {
+  const Aula09({super.key});
+
+  @override
+  State<Aula09> createState() => _Aula09State();
+}
+
+class _Aula09State extends State<Aula09> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Aula 09')),
+    );
+  }
+}
